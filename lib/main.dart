ebcb31c2c6d0db91067cfdf5de8bfd05d8768fd4
NilsDunlop <gusdunlni@student.gu.se>
2025-09-18 14:21:32 +0200

    Add main functionality
    Added task handling, sorting of tasks, completing and removing tasks

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
-  // Sample todo items
   List<Map<String, dynamic>> todoItems = [
     {'text': 'Write a book', 'isCompleted': false},
     {'text': 'Do homework', 'isCompleted': false},
@@ -38,6 +37,39 @@ class _TodoListScreenState extends State<TodoListScreen> {
     {'text': 'Have fun', 'isCompleted': false},
     {'text': 'Meditate', 'isCompleted': false},
   ];
+
+  String currentFilter = 'all';
+
+  void addTodoItem(String text) {
+    if (text.isNotEmpty) {
+      setState(() {
+        todoItems.add({'text': text, 'isCompleted': false});
+      });
+    }
+  }
+
+  void toggleTodoCompletion(int index) {
+    setState(() {
+      todoItems[index]['isCompleted'] = !todoItems[index]['isCompleted'];
+    });
+  }
+
+  void deleteTodoItem(int index) {
+    setState(() {
+      todoItems.removeAt(index);
+    });
+  }
+
+  List<Map<String, dynamic>> getFilteredTodos() {
+    switch (currentFilter) {
+      case 'done':
+        return todoItems.where((item) => item['isCompleted'] == true).toList();
+      case 'undone':
+        return todoItems.where((item) => item['isCompleted'] == false).toList();
+      default:
+        return todoItems;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,12 +89,14 @@ class _TodoListScreenState extends State<TodoListScreen> {
           PopupMenuButton<String>(
             icon: Icon(Icons.more_vert, color: Colors.white),
             onSelected: (String value) {
-              print('Selected: $value');
+              setState(() {
+                currentFilter = value;
+              });
             },
             itemBuilder: (BuildContext context) => [
-              PopupMenuItem(value: 'All', child: Text('all')),
-              PopupMenuItem(value: 'Done', child: Text('done')),
-              PopupMenuItem(value: 'Undone', child: Text('undone')),
+              PopupMenuItem(value: 'all', child: Text('All')),
+              PopupMenuItem(value: 'done', child: Text('Done')),
+              PopupMenuItem(value: 'undone', child: Text('Undone')),
             ],
           ),
         ],
@@ -71,8 +105,10 @@ class _TodoListScreenState extends State<TodoListScreen> {
         color: Colors.grey[100],
         child: ListView.builder(
           padding: EdgeInsets.all(16),
-          itemCount: todoItems.length,
+          itemCount: getFilteredTodos().length,
           itemBuilder: (context, index) {
+            List<Map<String, dynamic>> filteredTodos = getFilteredTodos();
+            int originalIndex = todoItems.indexOf(filteredTodos[index]);
             return Container(
               margin: EdgeInsets.only(bottom: 8),
               padding: EdgeInsets.all(16),
@@ -91,40 +127,43 @@ class _TodoListScreenState extends State<TodoListScreen> {
               child: Row(
                 children: [
                   // Checkbox
-                  Container(
-                    width: 24,
-                    height: 24,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: todoItems[index]['isCompleted'] 
+                  GestureDetector(
+                    onTap: () => toggleTodoCompletion(originalIndex),
+                    child: Container(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: filteredTodos[index]['isCompleted'] 
+                              ? Color(0xFF1E5A8A) 
+                              : Colors.grey[400]!, 
+                          width: 2
+                        ),
+                        shape: BoxShape.circle,
+                        color: filteredTodos[index]['isCompleted'] 
                             ? Color(0xFF1E5A8A) 
-                            : Colors.grey[400]!, 
-                        width: 2
+                            : Colors.transparent,
                       ),
-                      shape: BoxShape.circle,
-                      color: todoItems[index]['isCompleted'] 
-                          ? Color(0xFF1E5A8A) 
-                          : Colors.transparent,
+                      child: filteredTodos[index]['isCompleted']
+                          ? Icon(
+                              Icons.check,
+                              size: 16,
+                              color: Colors.white,
+                            )
+                          : null,
                     ),
-                    child: todoItems[index]['isCompleted']
-                        ? Icon(
-                            Icons.check,
-                            size: 16,
-                            color: Colors.white,
-                          )
-                        : null,
                   ),
                   SizedBox(width: 16),
                   // Task text
                   Expanded(
                     child: Text(
-                      todoItems[index]['text'],
+                      filteredTodos[index]['text'],
                       style: TextStyle(
                         fontSize: 16,
-                        decoration: todoItems[index]['isCompleted']
+                        decoration: filteredTodos[index]['isCompleted']
                             ? TextDecoration.lineThrough
                             : TextDecoration.none,
-                        color: todoItems[index]['isCompleted']
+                        color: filteredTodos[index]['isCompleted']
                             ? Colors.grey[600]
                             : Colors.black,
                       ),
@@ -132,9 +171,7 @@ class _TodoListScreenState extends State<TodoListScreen> {
                   ),
                   // Delete button
                   GestureDetector(
-                    onTap: () {
-                      print('Delete item: ${todoItems[index]['text']}');
-                    },
+                    onTap: () => deleteTodoItem(originalIndex),
                     child: Icon(
                       Icons.close,
                       color: Colors.grey[600],
@@ -158,7 +195,9 @@ class _TodoListScreenState extends State<TodoListScreen> {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => AddTaskScreen()),
+              MaterialPageRoute(
+                builder: (context) => AddTaskScreen(onAddTask: addTodoItem),
+              ),
             );
           },
           backgroundColor: Colors.transparent,
@@ -175,6 +214,10 @@ class _TodoListScreenState extends State<TodoListScreen> {
 }
 
 class AddTaskScreen extends StatefulWidget {
+  final Function(String) onAddTask;
+
+  const AddTaskScreen({super.key, required this.onAddTask});
+
   @override
   State<AddTaskScreen> createState() => _AddTaskScreenState();
 }
@@ -229,8 +272,20 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                print('Add task: ${_textController.text}');
-                Navigator.pop(context);
+                String taskText = _textController.text.trim();
+                if (taskText.isNotEmpty) {
+                  widget.onAddTask(taskText);
+                  _textController.clear();
+                  
+                  // Show a task added confirmation
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Task added successfully!'),
+                      duration: Duration(seconds: 1),
+                      backgroundColor: Color(0xFF1E5A8A),
+                    ),
+                  );
+                }
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFF1E5A8A),
